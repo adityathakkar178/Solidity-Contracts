7a606f6a605f61abb10976eefbc0d0a7d4c9104d
adityathakkar178 <160093854+adityathakkar178@users.noreply.github.com>
2024-02-28 11:18:41 +0530

    OpenzeppelinERC721 contract

--- OpenzeppelinERC721.sol
+++ OpenzeppelinERC721.sol
@@ -7,30 +7,31 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract MyERC721 is ERC721, ERC721URIStorage{
     uint256 private _tokenIdCounter = 0;
     string private _baseUri;
-    mapping (uint256 => string) private _tokenURIs; 
-
+    mapping (string => bool) private _tokenURIs;
+    
     constructor() ERC721("My Token", "MTK") {}
 
     function mint(string memory _tokenName, string memory _tokenURI) public {
         require(bytes(_tokenName).length > 0 && bytes(_tokenURI).length > 0, "Token name, Token id, Token URI can not be empty");
+        require(!_tokenURIs[_tokenURI], "Token URI already exists");
+        _tokenURIs[_tokenURI] = true;
         _tokenIdCounter++;
         uint256 tokenId = _tokenIdCounter;
         _mint(msg.sender, tokenId);
         _setTokenURI(tokenId, _tokenURI);
     }
 
-    function setBaseURI(string memory _baseURI) public {
-        _baseUri = _baseURI;
+    function _baseURI() internal view override returns(string memory) {
+        return _baseUri;
     }
+
+    function setBaseURI(string memory _baseTokenURI) public {
+        _baseUri = _baseTokenURI;
+    } 
 
     function tokenURI(uint256 _tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(_tokenId);
     }
-
-    function getURI(uint256 _tokenId) public view returns(string memory) {
-        require(ownerOf(_tokenId) != address(0), "Token Id does not exist");
-        return string(abi.encodePacked(_baseUri, tokenURI(_tokenId)));
-    } 
     
     function supportsInterface(bytes4 _interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool){
         return super.supportsInterface(_interfaceId);
