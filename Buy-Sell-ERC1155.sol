122aa5d16b0220c6619002cd2929d339d9130190
adityathakkar178 <160093854+adityathakkar178@users.noreply.github.com>
2024-03-07 11:21:59 +0530

    Buy-Sell-ERC1155 contract

--- Buy-Sell-ERC1155.sol
+++ Buy-Sell-ERC1155.sol
@@ -22,7 +22,7 @@ contract MyERC1155 is ERC1155{
     uint256 private _commissionRate;
     mapping (string => bool) private _uris;
     mapping (uint256 => Creators) public creator;
-    mapping (uint256=>Seller) public saleToken;
+    mapping (uint256=>Seller[]) public saleToken;
 
     constructor(uint256 _price, uint256 _adminCommissionRate) ERC1155("") {
         _admin = msg.sender;
@@ -54,25 +54,29 @@ contract MyERC1155 is ERC1155{
     function sell(uint256 _tokenId, uint256 _amount, uint256 _price) public {
         require(balanceOf(msg.sender, 1) >=_amount, "Insufficient balance");
         require(_price > 0, "Price must be gerater than zero");
-        saleToken[_tokenId] = Seller(msg.sender, _tokenId, _amount, _price);
+        saleToken[_tokenId].push(Seller(msg.sender, _tokenId, _amount, _price));
     }
 
-    function buy(uint256 _tokenId) public payable{
-        require(saleToken[_tokenId].seller != address(0), "Token not for sell");
-        require(msg.value == saleToken[_tokenId].price, "Incorrect amount sent");
-        uint256 adminCommission = (saleToken[_tokenId].price * _commissionRate) / 100;
+    function getSellers(uint256 _tokenId) public view returns(Seller[] memory) {
+        return saleToken[_tokenId];
+    }
+
+    function buy(uint256 _tokenId, uint256 _seller) public payable{
+        require(saleToken[_tokenId].length > 0, "Token not for sell");
+        require(msg.value == saleToken[_tokenId][_seller].price, "Incorrect amount sent");
+        uint256 adminCommission = (saleToken[_tokenId][_seller].price * _commissionRate) / 100;
         uint256 royalty = 0;
-        if(saleToken[_tokenId].seller != creator[_tokenId].actualOwner) {
-            royalty = (saleToken[_tokenId].price * creator[_tokenId].royaltyRate) / 100;
+        if(saleToken[_tokenId][_seller].seller != creator[_tokenId].actualOwner) {
+            royalty = (saleToken[_tokenId][_seller].price * creator[_tokenId].royaltyRate) / 100;
         }
         uint256 remainingAmount = msg.value - (adminCommission + royalty);
-        _safeTransferFrom(saleToken[_tokenId].seller, msg.sender, _tokenId, saleToken[_tokenId].amount, "");
+        _safeTransferFrom(saleToken[_tokenId][_seller].seller, msg.sender, _tokenId, saleToken[_tokenId][_seller].amount, "");
         if (royalty > 0) {
             payable(creator[_tokenId].actualOwner).transfer(royalty);
         }
-        payable(saleToken[_tokenId].seller).transfer(remainingAmount);
+        payable(saleToken[_tokenId][_seller].seller).transfer(remainingAmount);
         _adminCommission += adminCommission;
-        delete saleToken[_tokenId];
+        delete saleToken[_tokenId][_seller];
     }
 
     function withdraw() public {
